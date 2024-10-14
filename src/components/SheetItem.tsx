d3d2e1b8c07129511402b17f10a5f81523a9f560
Martín Gesualdo <mgesualdo14@gmail.com>
2024-10-14 20:19:33 -0300

    Cambios

--- src/components/SheetItem.tsx
+++ src/components/SheetItem.tsx
@@ -1,14 +1,7 @@
-import {
-  Home,
-  LineChart,
-  Package,
-  Package2,
-  PanelLeft,
-  ShoppingCart,
-  Users2,
-} from "lucide-react";
+import { Home, PanelLeft } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
+import AsideItem from "./AsideItem";
 
 const SheetItem = () => {
   return (
@@ -19,50 +12,12 @@ const SheetItem = () => {
           <span className="sr-only">Toggle Menu</span>
         </Button>
       </SheetTrigger>
-      <SheetContent side="left" className="sm:max-w-xs">
-        <nav className="grid gap-6 text-lg font-medium">
-          <a
-            href="#"
-            className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
-          >
-            <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
-            <span className="sr-only">Acme Inc</span>
-          </a>
-          <a
-            href="#"
-            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-          >
-            <Home className="h-5 w-5" />
-            Dashboard
-          </a>
-          <a
-            href="#"
-            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-          >
-            <ShoppingCart className="h-5 w-5" />
-            Orders
-          </a>
-          <a
-            href="#"
-            className="flex items-center gap-4 px-2.5 text-foreground"
-          >
-            <Package className="h-5 w-5" />
-            Products
-          </a>
-          <a
-            href="#"
-            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-          >
-            <Users2 className="h-5 w-5" />
-            Customers
-          </a>
-          <a
-            href="#"
-            className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-          >
-            <LineChart className="h-5 w-5" />
-            Settings
-          </a>
+      <SheetContent side="left" className="sm:max-w-xs max-w-44">
+        <nav className="flex flex-col items-start gap-4 px-2 py-4">
+          <AsideItem text="Ventas" href="/" icon="washes" tooltip={false} />
+          <AsideItem text="Servicios" icon="services" tooltip={false} />
+          <AsideItem text="Clientes" icon="clients" tooltip={false} />
+          <AsideItem text="Informes" icon="dashboard" tooltip={false} />
         </nav>
       </SheetContent>
     </Sheet>
