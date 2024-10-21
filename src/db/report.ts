454ec413fdd3b008675ffadfc19dd39629338b41
Martín Gesualdo <mgesualdo14@gmail.com>
2024-10-21 12:35:22 -0300

    Cambios

--- src/db/report.ts
+++ src/db/report.ts
@@ -1,5 +1,12 @@
 import { db } from ".";
 
-export const getReports = async () => {
-  return await db.query.daily_reports.findMany();
+export const getReports = async (
+  report: "sale" | "cashflow"
+): Promise<any[]> => {
+  if (report === "sale") return await db.query.sales_daily_report.findMany();
+
+  if (report === "cashflow")
+    return await db.query.cashflows_daily_report.findMany();
+
+  return await db.query.cashflows_daily_report.findMany();
 };
--- src/schemas/report.ts
+++ src/schemas/report.ts
@@ -6,24 +6,20 @@ import {
   uniqueIndex,
 } from "drizzle-orm/sqlite-core";
 
-export const daily_reports = sqliteTable(
-  "DailyReport",
+export const sales_daily_report = sqliteTable(
+  "SalesDailyReport",
   {
     id: integer("id").primaryKey(),
     day: integer("day").notNull(),
     month: integer("month").notNull(),
     year: integer("year").notNull(),
     company_id: integer("company_id").notNull(),
-    sales_amount: real("sales_amount"),
-    sales_gathered: real("sales_gathered"),
-    sales: integer("sales"),
-    clients: integer("clients"),
-    vehicles: integer("vehicles"),
-    created_at: text("created_at"),
+    amount: real("amount"),
+    quantity: integer("quantity"),
   },
   (table) => {
     return {
-      unq_entry: uniqueIndex("unq_enty").on(
+      unq_entry: uniqueIndex("unq_entry_sales_report").on(
         table.day,
         table.month,
         table.year,
@@ -33,5 +29,33 @@ export const daily_reports = sqliteTable(
   }
 );
 
-export type InsertDailyReport = typeof daily_reports.$inferInsert;
-export type SelectDailyReport = typeof daily_reports.$inferSelect;
+export type InsertSaleDailyReport = typeof sales_daily_report.$inferInsert;
+export type SelectSaleDailyReport = typeof sales_daily_report.$inferSelect;
+
+export const cashflows_daily_report = sqliteTable(
+  "CashflowsDailyReport",
+  {
+    id: integer("id").primaryKey(),
+    day: integer("day").notNull(),
+    month: integer("month").notNull(),
+    year: integer("year").notNull(),
+    company_id: integer("company_id").notNull(),
+    amount: real("amount"),
+    quantity: integer("quantity"),
+    method: text("method"),
+  },
+  (table) => {
+    return {
+      unq_entry: uniqueIndex("unq_entry_cashflows_report").on(
+        table.day,
+        table.month,
+        table.year,
+        table.method,
+        table.company_id
+      ),
+    };
+  }
+);
+
+export type InsertCashflowDailyReport = typeof sales_daily_report.$inferInsert;
+export type SelectCashflowDailyReport = typeof sales_daily_report.$inferSelect;
